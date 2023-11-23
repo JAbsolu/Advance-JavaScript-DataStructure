f72053fac70158781431b03fb447444429369918
Johnson Absolu <Johnson.absolu@outlook.com>
2023-11-23 01:46:31 -0500

    ---- Added all trees traversal methods ----

--- Trees/index.js
+++ Trees/index.js
@@ -36,42 +36,108 @@ class BinaryTree {
             this.right = new_node
         }
     }
-    //print the tree on the console
-    stringValue(){
+    //print the tree on the console using depth first traversal
+    depth_first(){
         const stack = [this];
-        let index = 1;
 
         while (stack.length > 0){
             const current = stack.pop()
+            console.log(current.key)
 
-            if (current.getLeftChild() !== null) {
-                stack.push(current.getLeftChild())
-                console.log(`Node ${index}'s left `, current.getLeftChild().getVal(), `\n`)
+            if (current.left !== null) {
+                stack.push(current.left)
             }
 
-            if (current.getRightChild() !== null) {
-                stack.push(current.getRightChild())
-                console.log(`Node ${index}'s right `, current.getRightChild().getVal(), `\n`)
+            if (current.right!== null) {
+                stack.push(current.right)
             }
-
-            index+=1;
         }
     }
+
+    // print tree using breadth first traversal
+    breadth_first(){
+        const queue = [this]
+
+        while (queue.length > 0) {
+            const current = queue.shift();
+            console.log(current.key)
+
+            if (current.left) {
+                queue.push(current.left)
+            }
+
+            if (current.right) {
+                queue.push(current.right)
+            }
+        }
+    }
+
+   
 
 }
 
 //Build a tree
-const list = ['John', 'Nick', "Jeff", 'Anne', "Marry", "Bane", "Marie", "Jane Doe"];
+const list = ['John', 'Nick', "Jeff", 'Anne', "Marry", "Bane", "Marie", "Jane Doe", "Ben Doe", "Doe Man", "Google", "Amazon", "Apple"];
 const my_binary_tree = new BinaryTree(list[0]);
 
 const buildTree = (list) => {
     for (let i = 1; i < list.length; i+=1) {
         my_binary_tree.insertLeftChild(list[i])
-        my_binary_tree.insertRightChild(list[i+1])
+        if (list[i+1]) my_binary_tree.insertRightChild(list[i+1])
     }
-    console.log(my_binary_tree.stringValue()); 
-    console.log(my_binary_tree);
-    return my_binary_tree;
+    return my_binary_tree.depth_first();
 }
 
 buildTree(list);
+
+
+console.log(`\n${'='.repeat(50)}`);
+console.log('Breadth first print result\n');
+
+my_binary_tree.breadth_first();
+
+
+
+ //print the tree using preorder traversal
+ console.log(`\n${'='.repeat(50)}`);
+ console.log('Preorder print result\n');
+
+ const preorder = (tree) => {
+    if (tree) {
+        preorder(tree.left)
+        console.log(tree.key)
+        preorder(tree.right)
+    }
+};
+preorder(my_binary_tree);
+
+
+
+ //print the tree using inorder traversal
+console.log(`\n${'='.repeat(50)}`);
+console.log('Inorder print result\n');
+
+const inorder = (tree) => {
+    if (tree) {
+        inorder(tree.left)
+        console.log(tree.key)
+        inorder(tree.right)
+    }
+}
+inorder(my_binary_tree)
+
+
+
+ //print the tree using postorder traversal
+console.log(`\n${'='.repeat(50)}`);
+console.log('Postorder print result\n');
+
+const postorder = (tree) => {
+    if (tree) {
+        postorder(tree.left)
+        console.log(tree.key)
+        postorder(tree.right)
+    }
+}
+
+postorder(my_binary_tree)
