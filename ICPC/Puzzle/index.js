d4ad3627c18cbc4ee2d689279e12331b34897c57
Absolu <absoluj1@southernct.edu>
2024-03-20 19:24:08 -0400

    Updated ICPC

--- ICPC/Puzzle/index.js
+++ ICPC/Puzzle/index.js
@@ -1,7 +1,10 @@
-class Puzzle {
-    constructor (board, flip) {
+const readline = require("readline");
+
+class SnakeLadder {
+    constructor (board, flip, start) {
         this.flip = this.flipCoin();
         this.board = this.buildBoard();
+        this.start = this.start();
     }
 
     //Build the board
@@ -31,9 +34,23 @@ class Puzzle {
         if (coin === 1) return "Coin: Head"
         else return "Coin: Tail";
     }
+
+    //Start the game and prompt something to the user {test}
+    start() {
+        const input = readline.createInterface(process.stdin, process.stdout);
+        //Ask the user to chose an option
+        input.question(`Enter Y to continue, or N to cancel: `, (option) => {
+            if (option !== 'Y' || "Yes") {
+                console.log("Wrong input!, please enter either Y, N or Yes or No.");
+                this.start();
+            }
+        });
+
+    }
 }  
 
 
-const game = new Puzzle();
-console.log(game.flip);
-console.log(game.board);
+const game = new SnakeLadder();
+console.log(game.start)
+// console.log(game.flip);
+// console.log(game.board);
